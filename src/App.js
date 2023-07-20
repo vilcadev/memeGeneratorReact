9b8a6c28418b880afb6d663de056cce8186dc9dd
cesar <vilca@vilcadev.com>
2023-07-20 15:13:29 -0500

    react first save

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  var nombre = "Cesar";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hola Mundo soy {nombre}<br/>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
